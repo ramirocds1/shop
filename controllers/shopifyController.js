f49e42d6bd4d949f32df8ab722f862482f955ffd
Ramiro Rodriguez <rrodriguez@codigodelsur.com>
2016-07-26 14:06:13 -0300

    fas

--- controllers/shopifyController.js
+++ controllers/shopifyController.js
@@ -96,7 +96,8 @@ exports.updateOrder = function (req, res) {
 	//get fullfilment for order with order number
 
 	console.log("llega");
-
+	console.log(req.body.form.key);
+	console.log(req.form.key);
 	res.json({
 		message: 'OK'
 	});
