615a63d22e9ba6e22fdeafeb66b3354564325728
Ramiro Rodriguez <rrodriguez@codigodelsur.com>
2016-08-05 10:47:57 -0300

    test

--- includes/customer.js
+++ includes/customer.js
@@ -2,78 +2,82 @@ var performRequest2 = require('./performRequest2');
 
 exports.saveCustomer = function  (infoReturned, cb){
 
-if ( infoReturned.bodyGetCustomerDetails == "NOT_FOUND" )
-	console.log("NO_FOUND")
-else console.log("YES_FOUND")
+	if ( infoReturned.bodyGetCustomerDetails.existence == "NOT_FOUND" ){
+		console.log("not found: " , infoReturned.bodyGetCustomerDetails.existence)
 /*
-	var customer = infoReturned['shopifyInfo'].customer
-	var billing_address = customer.billing_address
+		var customer = infoReturned['shopifyInfo'].customer
+		var billing_address = customer.billing_address
+		var code = infoReturned['bodyGetCustomerDetails'].custCode;
 
-	var customerData = `{
-		key: [{ "API_KEY": "`+infoReturned['API_KEY']+`",
-				"SESSION_KEY": "`+infoReturned['SESSION_KEY']+`"}],
-		data:"{
-				'oCustomer':{
-								'Active':null,
-								'CustCode':'',
-								'Name':'`+ customer.name +`',
-								'CustomerNotes':'`+ customer.note +`',
-								'SourceCode':'',
-								'PONumberRequired':null,
-								'DefaultShipCode':`+ customer.shipping_address.zip +`,
-								'DefaultContCode':0,
-								'ShippingAddrCount':0,
-								'ContactAddrCount':0,
-								'BillingAddressRecord':{
-															'Active':null,
-															'AddressCode':0,
-															'CustCode':null,
-															'Password':'test',
-															'CompanyName':'',
-															'FirstName':'`+ billing_address.first_name +`',
-															'LastName':'`+ billing_address.last_name +`',
-															'FirstLast':null,
-															'Street':'`+ billing_address.address1 +`',
-															'Email':'`+ customer.email +`',
-															'SubType':'2',
-															'Telephone':'`+ billing_address.phone +`',
-															'Telephone2':null,
-															'Fax':'',
-															'City':'`+ billing_address.city +`',
-															'State':'`+ billing_address.province_code +`',
-															'Zip':'`+ billing_address.zip +`',
-															'Country':'`+ billing_address.country +`',
-															'SecretQuestion':'',
-															'SecretAnswer':'',
-															'Residence':'F',
-															'AddressType':0,
-															'IsAddressUsed':false,
-															'FullAddress':null,
-															'MobilePhone':null
-														},
-														'ShippingAddressRecords':[],
-														'ContactAddressRecords':[]
+		var customerData = `{
+			key: [{ "API_KEY": "`+infoReturned['API_KEY']+`",
+					"SESSION_KEY": "`+infoReturned['SESSION_KEY']+`"}],
+			data:"{
+					'oCustomer':{
+									'Active':null,
+									'CustCode':'`+code+`',
+									'Name':'`+ customer.name +`',
+									'CustomerNotes':'`+ customer.note +`',
+									'SourceCode':'',
+									'PONumberRequired':null,
+									'DefaultShipCode':`+ customer.shipping_address.zip +`,
+									'DefaultContCode':0,
+									'ShippingAddrCount':0,
+									'ContactAddrCount':0,
+									'BillingAddressRecord':{
+														'Active':null,
+														'AddressCode':0,
+														'CustCode':null,
+														'Password':'test',
+														'CompanyName':'',
+														'FirstName':'`+ billing_address.first_name +`',
+														'LastName':'`+ billing_address.last_name +`',
+														'FirstLast':null,
+														'Street':'`+ billing_address.address1 +`',
+														'Email':'`+ customer.email +`',
+														'SubType':'2',
+														'Telephone':'`+ billing_address.phone +`',
+														'Telephone2':null,
+														'Fax':'',
+														'City':'`+ billing_address.city +`',
+														'State':'`+ billing_address.province_code +`',
+														'Zip':'`+ billing_address.zip +`',
+														'Country':'`+ billing_address.country +`',
+														'SecretQuestion':'',
+														'SecretAnswer':'',
+														'Residence':'F',
+														'AddressType':0,
+														'IsAddressUsed':false,
+														'FullAddress':null,
+														'MobilePhone':null
 													},
-								'isNew':'1',
-								'regisError':'',
-								'arrCustSurvey':'[]'
-							}"
-			}`;
+													'ShippingAddressRecords':[],
+													'ContactAddressRecords':[]
+														},
+									'isNew':'1',
+									'regisError':'',
+									'arrCustSurvey':'[]'
+								}"
+				}`;
 
 
-	performRequest2.performRequest( 'POST','/StoreAPI/AccountMngmnt/SaveCustomer',customerData,
-		function (body) {
-			console.log("saveCustomer OK");
-			//console.log(body);
-			cb(null,body);
-		},
-		function (body) {
-			console.log("saveCustomer Error");
-			//console.log(body);
-			cb(1,body);
-		}
-	);
+		performRequest2.performRequest( 'POST','/StoreAPI/AccountMngmnt/SaveCustomer',customerData,
+			function (body) {
+				console.log("saveCustomer OK");
+				//console.log(body);
+				cb(null,body);
+			},
+			function (body) {
+				console.log("saveCustomer Error");
+				//console.log(body);
+				cb(1,body);
+			}
+		);
 */
+	}
+
+
+
 }
 
 exports.getCustomerDetails = function  (infoReturned, cb){
@@ -94,7 +98,7 @@ exports.getCustomerDetails = function  (infoReturned, cb){
 		function (body) {
 			console.log("getCustomerDetails Error, Customer does not exist");
 			//console.log(body);
-			cb(null, "NOT_FOUND" );
+			cb( null, { existence: "NOT_FOUND" , custCode: custCode } );
 		}
 	);
 
