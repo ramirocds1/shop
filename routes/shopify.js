b705875f846f712463c0687d974a0bb315abfd4c
Ramiro Rodriguez <rrodriguez@codigodelsur.com>
2016-08-08 15:10:17 -0300

    test

--- routes/shopify.js
+++ routes/shopify.js
@@ -3,6 +3,5 @@ var router = express.Router();
 var shopifyCtrl = require('../controllers/shopifyController');
 
 router.post('/orderPlaced', shopifyCtrl.orderPlaced)
-router.post('/updateOrder', shopifyCtrl.updateOrder)
 
 module.exports = router;
