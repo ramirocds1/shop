697c1cf8df6e458cc9091f0fa2b1aae4114807b3
Ramiro Rodriguez <rrodriguez@codigodelsur.com>
2016-08-12 15:47:03 -0300

    test

--- includes/performRequest.js
+++ includes/performRequest.js
@@ -1,12 +1,11 @@
 var nconf    = require('nconf');
 var request = require('request');
-var host = nconf.get("additionalKeys:gsBackoffice");
 
 // ***** FOR MAKING API CALLS *****
 exports.performRequestLogin =  function( method , endpoint, data, cb, cbError) {
-	console.log ("\nCalling: " + method + " " + host + endpoint);
+	console.log ("\nCalling: " + method + " " + nconf.get("additionalKeys:gsBackoffice") + endpoint);
 	request.post( 
-					{ 	url: host + endpoint,
+					{ 	url: nconf.get("additionalKeys:gsBackoffice") + endpoint,
 						form: data,
 						headers: { 
 									'cache-control': 'no-cache',
@@ -32,7 +31,7 @@ exports.performRequest =  function( method , endpoint, data, cb, cbError) {
 	request(
 				{
 					method: 'POST',
-					url: host + endpoint,
+					url: nconf.get("additionalKeys:gsBackoffice") + endpoint,
 					headers: {
 								'cache-control': 'no-cache',
 								'content-type': 'application/json'
@@ -41,7 +40,7 @@ exports.performRequest =  function( method , endpoint, data, cb, cbError) {
 				},
 
 				function callback(error, response, body) {
-				  	  var msj = "\nCalling: " + method + " " + host + endpoint;
+				  	  var msj = "\nCalling: " + method + " " + nconf.get("additionalKeys:gsBackoffice") + endpoint;
 					  if (!error && response.statusCode == 200) {
 					  		console.log(msj + " -> RETURNED OK.");
 					    	cb(body);
